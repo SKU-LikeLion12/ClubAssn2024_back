ad55898a3a29a504e207721c7dbbed52fa511d73
un-known0 <nam0221@naver.com>
2024-02-22 16:44:50 +0900

    [init] puzzle project initialize

---
+++ src/main/java/likelion12/puzzle/PuzzleApplication.java
@@ -0,0 +1,13 @@
+package likelion12.puzzle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuzzleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuzzleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/likelion12/puzzle/PuzzleApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion12.puzzle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuzzleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
