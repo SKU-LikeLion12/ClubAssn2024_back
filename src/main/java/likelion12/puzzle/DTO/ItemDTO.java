ee5b40c20be4bfd78ca1480b7b7a520dacc96823
chtw2001 <jungtaekwon1019@gmail.com>
2024-03-01 09:07:07 +0900

    [add] item. Controller, DTO added and fix some item. Service, Repository. added ImageUtility

---
+++ src/main/java/likelion12/puzzle/DTO/ItemDTO.java
@@ -0,0 +1,29 @@
+package likelion12.puzzle.DTO;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+public class ItemDTO {
+    @Data
+    @AllArgsConstructor
+    public static class ItemCreateRequest{
+        @NotEmpty
+        private String name;
+        @NotEmpty
+        private int count;
+        @NotEmpty
+        private String image;
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class ItemAllRequest {
+        private Long id;
+        private String name;
+        private int count;
+    }
+
+}
+
--- src/main/java/likelion12/puzzle/domain/Item.java
+++ src/main/java/likelion12/puzzle/domain/Item.java
@@ -1,15 +1,16 @@
 package likelion12.puzzle.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
+@Setter
 public class Item {
 
     @Id @GeneratedValue @Column(name="item_id")
@@ -18,9 +19,15 @@ public class Item {
     String name;
     Integer count;
 
-    public Item(String name, int count){
+    // 이미지를 담는 byte 배열을 BLOB(Binary Large Object) 형식으로 저장
+    @Lob
+    @Column(name = "image", columnDefinition = "MEDIUMBLOB") // TINYBLOB: ~255Byte BLOB: ~64KB  MEDIUMBLOB: ~16MB LONGBLOB: ~4GB
+    private byte[] image;
+
+    public Item(String name, int count, byte[] image){
         this.name = name;
         this.count = count;
+        this.image = image;
     }
 
 }
