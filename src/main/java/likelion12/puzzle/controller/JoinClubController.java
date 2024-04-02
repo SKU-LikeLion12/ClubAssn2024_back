7b5dba1ab015902303344c31c1268c24f781e491
han-mg <alsrb1277@gmail.com>
2024-04-03 01:10:01 +0900

    [add] JoinClubController swagger description without part of it

--- src/main/java/likelion12/puzzle/controller/JoinClubController.java
+++ src/main/java/likelion12/puzzle/controller/JoinClubController.java
@@ -1,5 +1,7 @@
 package likelion12.puzzle.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import likelion12.puzzle.domain.JoinClub;
 import likelion12.puzzle.service.JoinClubService;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +20,9 @@ public class JoinClubController {
 
     private final JoinClubService joinClubService;
 
+    @Operation(summary = "동아리원 추가", description = "", tags = {"manage"},
+            responses = {@ApiResponse(responseCode = "201", description = "생성 성공 후 joinClub 객체 반환"),
+                    @ApiResponse(responseCode = "", description = "")})
     @PostMapping("/member/manage/add")
     public ResponseEntity<JoinClub> addNewMember(@RequestBody CreateJC request) {
         JoinClub joinClub = joinClubService.saveNewMember(request.getStudentId(), request.getStudentName(), request.getClubName());
@@ -30,12 +35,18 @@ public class JoinClubController {
 //        }
     }
 
+    @Operation(summary = "동아리원 삭제", description = "학번과 동아리 이름을 받아 동아리원 삭제", tags = {"manage"},
+            responses = {@ApiResponse(responseCode = "204", description = "삭제 성공 후 삭제 완료 메시지 반환"),
+                    @ApiResponse(responseCode = "", description = "")})
     @DeleteMapping("/member/manage")
     public ResponseEntity<String> deleteClubMember(@RequestBody DeleteJC request) {
         joinClubService.deleteJoinClub(request.getStudentId(), request.getClubName());
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body("삭제 완료");
     }
 
+    @Operation(summary = "동아리원 검색", description = "학번, 이름, 동아리로 검색하면 알맞은 동아리원 정보가 나옴", tags = {"manage"},
+            responses = {@ApiResponse(responseCode = "200", description = "검색 성공 후 학번, 성함, 동아리명 반환"),
+                    @ApiResponse(responseCode = "204", description = "없는 정보를 입력하면 요청에 대해 보내줄 콘텐츠가 없음")})
     @GetMapping("/member/manage")
     public ResponseEntity<List<CreateJC>> CMManageSearch(@RequestParam String keyword) {
 //        if (keyword == null || keyword.trim().isEmpty()) {
