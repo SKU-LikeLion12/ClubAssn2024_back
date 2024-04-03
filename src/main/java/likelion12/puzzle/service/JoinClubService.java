db49bcf1eeebf597185d02fe0c845349074a8a5d
un-known0 <nam0221@naver.com>
2024-04-03 16:24:39 +0900

    [fix] setRendomIconClub을 JoinClubService로 이동

--- src/main/java/likelion12/puzzle/service/JoinClubService.java
+++ src/main/java/likelion12/puzzle/service/JoinClubService.java
@@ -5,6 +5,7 @@ import likelion12.puzzle.DTO.MemberClubDTO.*;
 import likelion12.puzzle.domain.Club;
 import likelion12.puzzle.domain.JoinClub;
 import likelion12.puzzle.domain.Member;
+import likelion12.puzzle.exception.NoJoinedClubException;
 import likelion12.puzzle.repository.JoinClubRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,7 +18,6 @@ import java.util.List;
 @Transactional(readOnly = true)
 public class JoinClubService {
     private final JoinClubRepository joinClubRepository;
-    private final ClubService clubService;
     private final MemberService memberService;
 
     // 동아리에 새로운 학생 추가
@@ -73,5 +73,14 @@ public class JoinClubService {
     // 특정 멤버의 가입동아리, 미가입 동아리 반환
     public MemberJoinedUnjoinedClubDTO findJoinedClubUnJoinedClub(String studentId){
         return joinClubRepository.findJoinedClubUnJoinedClub(studentId);
+    }
+
+    // 로그인을 위해서 member조회할 때 iconClub이 null이면 이 함수 사용할 것.
+    @Transactional
+    public void setRandomIconClub(String studentId){
+        List<Club> joinedClubs = findByStudentIdClub(studentId);
+        if (joinedClubs.isEmpty()){
+            throw new NoJoinedClubException("가입된 club 없음.");
+        }
     }
 }
