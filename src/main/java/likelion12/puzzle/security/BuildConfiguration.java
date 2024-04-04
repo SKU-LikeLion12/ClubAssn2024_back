bfd5581b3a6cfd8289127be2d2d90849b753d8dc
un-known0 <nam0221@naver.com>
2024-04-05 01:46:55 +0900

    [add] buildConfiguration 추가(bãprofile을 build로 실행시 swagger 요청 주소를 tehhttps로 하도록 주소 직접 지정)

---
+++ src/main/java/likelion12/puzzle/security/BuildConfiguration.java
@@ -0,0 +1,26 @@
+package likelion12.puzzle.security;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("build")
+public class BuildConfiguration {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("puzzle")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("https://test.sku-sku.com/").description("Default Server URL"));
+    }
+}
