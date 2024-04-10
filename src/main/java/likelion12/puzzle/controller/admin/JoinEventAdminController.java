c5f4d08559ec1901516f86352b5dd200c05bb0f8
hojooo <ghwn5833@gmail.com>
2024-04-11 00:20:55 +0900

    fix2

--- src/main/java/likelion12/puzzle/controller/admin/JoinEventAdminController.java
+++ src/main/java/likelion12/puzzle/controller/admin/JoinEventAdminController.java
@@ -41,7 +41,7 @@ public class JoinEventAdminController {
     // 회원 퍼즐 조각 추가 페이지(참여 안한거만 나오게)
     @Operation(summary = "(호주) 회원의 미참여 이벤트 리스트 조회", description = "관리자 토큰 필요, 파라미터에 학번 필요")
     @GetMapping("")
-    public ResponseEntity<List<ResponsePuzzleForNotPart>> pageForAddEvent(@RequestBody RequestMemberId request) {
+    public ResponseEntity<List<ResponsePuzzleForNotPart>> pageForAddEvent(@RequestParam RequestMemberId request) {
         List<ResponsePuzzleForNotPart> responseEvents = joinEventService.findNotPartEventsExceptImage(request.getStudentId());
 
         return ResponseEntity.ok(responseEvents);
